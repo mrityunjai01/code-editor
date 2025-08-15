6684c008283e16657638990f3af73dcdf25b5e2a
Mrityunjai Singh <mrityunjaisingh@Mrityunjais-Laptop.local>
2025-08-14 18:47:15 -0700

    separated  server from client

--- server/client.py
+++
@@ -1,9 +0,0 @@
-import uuid
-
-
-class Client:
-    def __init__(self, websocket, name, client_room, client_id=None):
-        self.client_id = str(uuid.uuid4()) if client_id is None else client_id
-        self.name = name
-        self.client_room = client_room
-        self.ws = websocket
